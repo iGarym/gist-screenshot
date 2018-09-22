b3d68853452280aeede8c701f79b1b3334bbb38b
gaoym <gaoym@mingyuanyun.com>
2018-09-22 20:25:15 +0800

    add alias config

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,23 @@
+const path = require('path');
 const rewireLess = require('react-app-rewire-less');
-const { injectBabelPlugin } = require('react-app-rewired');
+const rewireAliases = require('react-app-rewire-aliases');
+const { paths, injectBabelPlugin } = require('react-app-rewired');
+
+const resolve = dir => path.resolve(__dirname, `${paths.appSrc}/${dir}/`);
 
 module.exports = (config, env) => {
   config = injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
     config
   );
+
+  config = rewireAliases.aliasesOptions({
+    '@utils': resolve('utils'),
+    '@styles': resolve('styles'),
+    '@assets': resolve('assets'),
+    '@components': resolve('components')
+  })(config, env);
+
   config = rewireLess.withLoaderOptions({
     javascriptEnabled: true,
     modifyVars: {
