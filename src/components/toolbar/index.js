76be6aff131c3619f37395dbb3f42792aa01fa4c
Gary <igary.gym@gmail.com>
2018-09-29 00:23:44 +0800

    use new Select component

--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 
 import Export from '@components/export';
-import ModeSelect from '@components/mode-select';
-import ThemeSelect from '@components/theme-select';
+import SelectWithLabel from '@components/select-with-label';
 import FontSetting from '@components/font-setting';
 import StyleSetting from '@components/style-setting';
 import ExportSetting from '@components/export-setting';
@@ -11,14 +10,38 @@ import './index.less';
 
 class Toolbar extends Component {
   render() {
+    const {
+      mode,
+      modes,
+      theme,
+      themes,
+      onModeChange,
+      onThemeChange
+    } = this.props;
+
+    const modeProps = {
+      label: 'Mode',
+      value: mode,
+      options: modes,
+      placeholder: '请选择语言',
+      onChange: onModeChange
+    };
+    const themeProps = {
+      label: 'Theme',
+      value: theme,
+      options: themes,
+      placeholder: '请选择主题',
+      onChange: onThemeChange
+    };
+
     return (
       <div className="toolbar">
         <Row type="flex" justify="space-between">
           <Col span={6}>
-            <ThemeSelect />
+            <SelectWithLabel {...modeProps} />
           </Col>
           <Col span={6}>
-            <ModeSelect />
+            <SelectWithLabel {...themeProps} />
           </Col>
           <Col span={3}>
             <FontSetting />
