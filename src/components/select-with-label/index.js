d8020987aefd067e24862adde2a8a3f03030d948
Gary <igary.gym@gmail.com>
2018-09-29 00:23:23 +0800

    add new Select component

---
+++ src/components/select-with-label/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Select } from 'antd';
+
+const Option = Select.Option;
+
+class SelectWithLabel extends Component {
+  render() {
+    const { value, label, placeholder, options, onChange } = this.props;
+    const props = {
+      onChange,
+      placeholder,
+      notFoundContent: '暂无数据'
+    };
+
+    if (value) {
+      props.defaultValue = value;
+    }
+
+    return (
+      <div className="select-with-label">
+        {label}：
+        <Select {...props}>
+          {options.map(item => (
+            <Option value={item.id} key={item.id}>
+              {item.label}
+            </Option>
+          ))}
+        </Select>
+      </div>
+    );
+  }
+}
+
+export default SelectWithLabel;
